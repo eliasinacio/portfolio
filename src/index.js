d8a0a577968923180b14f7fd2f8fa2d0ee58566b
eliasinacio <0.elias.inacio@gmail.com>
2023-10-15 08:53:19 -0300

    feat: show current age

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 // calcular idade
-const age = 21
+// para mostrar sempre automaticamente minha idade correta
 
+const ageElement = document.querySelector('.age');
 
+const currentDate = new Date();
+const bornDate = new Date('2002,03,03');
 
+const age = currentDate.getTime() - bornDate.getTime();
+const ageInYears = Math.floor(age / 31556926000);
 
-
+ageElement.innerText = ageInYears;
